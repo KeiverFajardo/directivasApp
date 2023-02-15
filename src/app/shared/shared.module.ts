b6cb7600e56f709311119cd20c44a29a6269607f
keiver Fajardo <keiver.fajardo@fing.edu.uy>
2023-02-15 10:40:57 -0300

    directiva_personalizada_error-msg_and_msgIf

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CustomIfDirective } from './directives/custom-if.directive';
+import { ErrorMsgDirective } from './directives/error-msg.directive';
+
+
+@NgModule({
+  declarations: [
+    CustomIfDirective,
+    ErrorMsgDirective
+  ],
+  exports : [
+    CustomIfDirective,
+    ErrorMsgDirective
+  ]
+})
+export class SharedModule { }
